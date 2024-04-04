df3d634341dda1d21dd3c10df4142eff392e8992
Rameshwar Singh <rameshwar.job2@gmail.com>
2024-04-04 16:28:53 -0400

    Generate charts using titanic dataset

--- seaborn.py
+++ seaborn.py
@@ -85,6 +85,26 @@ sns.countplot(x='sex', data=tips)
 
 sns.lmplot(x='total_bill', y = 'tip', data =tips, hue= 'sex', palette='seismic')
 
+#Test on titanic data set
+sns.set_style('whitegrid')
+titanic = sns.load_dataset('titanic')
+titanic.head()
+sns.jointplot(x='fare', y='age', data= titanic)
+
+sns.distplot(titanic['fare'], kde=False, color='red', bins =30
+             
+sns.boxplot(x='class', y='age', data=titanic, palette = 'rainbow')
+
+sns.swarmplot(x='class', y='age', data=titanic, palette = 'Set2')
+
+sns.countplot(x='sex', data=titanic)
+
+sns.heatmap(titanic.corr(),cmap='coolwarm')
+plt.title('title.corr()')
+
+g=sns.FacetGrid(data =titanic, col= 'sex')
+g.map(plt.hist,'age')
+
 
 
 
